92165f36c3b6ea414cf733788ebf7bc13c7e6ec8
Koulinn <drdverzola@gmail.com>
2021-09-29 16:00:14 +0100

    Conditional to the post btn

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -11,25 +11,27 @@ export default class NavBar extends Component {
           <Navbar.Brand as={Link} to="/home">
             <img className="blog-navbar-brand" alt="logo" src={logo} />
           </Navbar.Brand>
-
-          <Button
-            as={Link}
-            to="/new"
-            className="blog-navbar-add-button bg-dark"
-            size="lg"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-plus-lg"
-              viewBox="0 0 16 16"
+          {this.props.isLogged ?
+            <Button
+              as={Link}
+              to="/new"
+              className="blog-navbar-add-button bg-dark"
+              size="lg"
             >
-              <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-            </svg>
-            Post Article
-          </Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-plus-lg"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+              </svg>
+              Post Article
+            </Button>
+
+            : ''}
         </Container>
       </Navbar>
     );
