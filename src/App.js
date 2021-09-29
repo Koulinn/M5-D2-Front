fc1989530aae41d8cb3f9c52b191a53dd1a65fd3
Koulinn <drdverzola@gmail.com>
2021-09-29 15:59:57 +0100

    Imported SignIn comp

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Home from "./views/home";
 import Blog from "./views/blog";
 import NewBlogPost from "./views/new";
 import SignupForm from "./views/signUp"
+import SignIn from "./views/signIn/SignIn";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import {useState} from 'react'
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false)
   return (
     <BrowserRouter>
-      <NavBar />
+      <NavBar isLogged={isLogged} />
       <Route exact path="/">
-         <Redirect to="/register" />
+         <Redirect to="/signIn" />
       </Route>
+      
+      <Route path="/signIn" exact render={()=><SignIn setIsLogged={setIsLogged}/>} />
       <Route path="/home" exact component={Home} />
       <Route path="/blog/:id" exact component={Blog} />
       <Route path="/new" exact component={NewBlogPost} />
